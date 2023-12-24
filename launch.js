c033a6fd832c762a21103e0a87a2751d82d86eeb
Alex Schwartz <alexschwartz01@gmail.com>
2023-12-24 10:36:30 -0500

    simpler

--- launch.js
+++ launch.js
@@ -1,40 +1,14 @@
-const CDP = require('chrome-remote-interface')
-
 const cp = require('child_process')
 const fs = require('fs')
 
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+fs.rmSync(__dirname + '/chromium-data', { recursive: true, force: true })
 
-function launchBrowser() {
-    const args = [
-        '--ignore-certificate-errors',
-        '--no-first-run',
-        '--proxy-server=http://localhost:' + process.argv[2],
-        '--remote-debugging-port=9222',
-        '--user-data-dir=' + __dirname + '/chromium-data',
-        '--disk-cache-dir=' + __dirname + '/chromium-data/DiskCache',
-    ]
-
-    cp.spawn('/usr/bin/chromium', args)
-}
-
-async function main() {
-    fs.rmSync(__dirname + '/chromium-data', { recursive: true, force: true })
-    launchBrowser()
-
-    await sleep(2000)
-
-    let client
-    try {
-        client = await CDP()
-        await client.Page.navigate({url: 'http://httpforever.com/__/#/specs/runner?file=cypress/e2e/spec.cy.js'})
-    } catch (err) {
-        console.error(err)
-    } finally {
-        if (client) {
-            await client.close()
-        }
-    }
-}
-
-main()
+const args = [
+    'http://httpforever.com/__/#/specs/runner?file=cypress/e2e/spec.cy.js',
+    '--ignore-certificate-errors',
+    '--no-first-run',
+    '--proxy-server=http://localhost:' + process.argv[2],
+    '--user-data-dir=' + __dirname + '/chromium-data',
+    '--disk-cache-dir=' + __dirname + '/chromium-data/DiskCache',
+]
+cp.spawn('/usr/bin/chromium', args)
